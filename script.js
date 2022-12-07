3bc87bfaab84c49b879f83de5f98f14ef7e4c265
Sean Buckle <39621011+seanbuckle@users.noreply.github.com>
2022-12-07 17:30:58 +0000

    Add variables for displaying the date

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-window.addEventListener("load", DisplayTime);
+window.addEventListener("load", displayTimeDate);
 
-function displayTime() {
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes()
-    let s = d.getSeconds();
+function displayTimeDate() {
+    const d,days,months,refresh;
+    let day,h,m,s,date,time;
+    d = new Date();
+    days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    day = days[d.getDay()];
+    date = d.getDate();
+    month = d.getMonth();
+    year = d.getFullYear();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
 
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
 
-    let time = h + ":" + m + ":" + s;
-    const refresh = 1000;
-    setTimeout('displayTime()', refresh);
+    date = day + ", " + date + " " + month + " " + year;
+    time = h + ":" + m + ":" + s;
+    refresh = 1000;
+    setTimeout('displayTimeDate()', refresh);
 
+    document.getElementById('date').innerHTML = date;
     document.getElementById('time').innerHTML = time;
 }
 function displayDate(){
