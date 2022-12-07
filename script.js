f7ec56f7f43d22042245bc020015b092d260be07
Sean Buckle <39621011+seanbuckle@users.noreply.github.com>
2022-12-07 00:55:35 +0000

    Fix time refresh

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function DisplayTime() {
     let d = new Date();
     let time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     const refresh = 1000;
-    mytime = setTimeout('time', refresh);
+    setTimeout('DisplayTime()', refresh);
 
     document.getElementById('time').innerHTML = time;
 }
