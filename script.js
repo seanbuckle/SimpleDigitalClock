e076d080d7cc5efe5b9c01be4cbac24099f66218
Sean Buckle <39621011+seanbuckle@users.noreply.github.com>
2022-12-07 14:00:01 +0000

    Add zero to time - numbers under 10

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ window.addEventListener("load", DisplayTime);
 
 function DisplayTime() {
     let d = new Date();
-    let time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    let h = d.getHours();
+    let m = d.getMinutes()
+    let s = d.getSeconds();
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s;
     const refresh = 1000;
     setTimeout('DisplayTime()', refresh);
 
