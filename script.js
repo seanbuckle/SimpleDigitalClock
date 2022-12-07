7952b88eb1a197c9c6de5b38722d7266354b6c6c
Sean Buckle <39621011+seanbuckle@users.noreply.github.com>
2022-12-07 00:37:49 +0000

    Fix date variable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", DisplayTime);
 
 function DisplayTime() {
-    let d = new date();
+    let d = new Date();
     let time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     const refresh = 1000;
     mytime = setTimeout('time', refresh);
