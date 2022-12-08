9b2a7b2c54cfb6f7c9fb64f15d19962e3a108811
Sean Buckle <39621011+seanbuckle@users.noreply.github.com>
2022-12-08 15:30:27 +0000

    add use strict

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 window.addEventListener("load", displayTimeDate = () => {
     const d = new Date();
     const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
