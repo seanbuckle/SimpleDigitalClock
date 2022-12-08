675647e72098c70d4269b871317654359ae8407f
Sean Buckle <39621011+seanbuckle@users.noreply.github.com>
2022-12-08 00:03:02 +0000

    Add twelve hour time

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ displayTimeDate = () => {
     const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
     const refresh = 1000;
-    let day,date,month,year,h,m,s,time;
+    let day,date,month,year,h,hTwelve,m,s,ampm,time;
 
     day = days[d.getDay()];
     date = d.getDate().toString();
@@ -12,28 +12,28 @@ displayTimeDate = () => {
     h = d.getHours();
     m = d.getMinutes();
     s = d.getSeconds();
+    ampm = h >= 12 ? " PM" : " AM";
 
     date = date.endsWith("1") ? date + "st"
         : date.endsWith("2") ? date + "nd"
         : date.endsWith("3") ? date + "rd"
         : date + "th";
-
+    
+    hTwelve = h % 12;
+    hTwelve = hTwelve < 10 ? "0" + hTwelve : hTwelve;
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
-
-    changeClock = () => {
-        let ampm = h >= 12 ? " PM" : " AM";
-        h = h % 12;
-        h = h ? h : 12;
-    }
+    
 
     date = day + ", " + date + " " + month + " " + year;
-    time = h + ":" + m + ":" + s + ampm;
+    time = h + ":" + m + ":" + s;
+    timeTwelve = h + ":" + m + ":" + s + ampm;
     setTimeout('displayTimeDate()', refresh);
 
     document.getElementById('date').innerHTML = date;
     document.getElementById('time').innerHTML = time;
+    document.getElementById('time-twelve').innerHTML = timeTwelve;
 }
 
 window.addEventListener("load", displayTimeDate);
